a5485af3779e12379009ad8d3379c32a2c4db4f6
michal7018 <michal7018@gmail.com>
2018-02-10 00:24:10 +0100

    commit

---
+++ WebApplication1/WebApplication1/ChatHub.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.IO;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace WebApplication1
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            Clients.All.broadcastMessage(name, message);
+
+        }
+    }
+
+
+
+}
