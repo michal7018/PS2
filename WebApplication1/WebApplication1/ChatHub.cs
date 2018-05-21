e1293e609a37dacfca6de4c41642e64580adf16c
michal7018 <michal7018@gmail.com>
2018-05-21 20:34:33 +0200

    tghf

--- WebApplication1/WebApplication1/ChatHub.cs
+++ WebApplication1/WebApplication1/ChatHub.cs
@@ -3,6 +3,7 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
+using System.Diagnostics;
 using System.IO;
 using System.Net.WebSockets;
 using System.Text;
@@ -63,10 +64,243 @@ namespace WebApplication1
 
 
 
+        public override Task OnConnected()
+        {
+            var name = Context.ConnectionId;
+            Debug.WriteLine(name.ToString() + "  connected");
+
+            return base.OnConnected();
+        }
+
+        public void getTablesList(string message, string conID)
+        {
+            List<object[]> response = getQuery(message);
+            Clients.Client(conID).getTables(response);
+        }
+
+        public void ReadSingleTable(string message, string conID)
+        {
+            List<object[]> response = getQuery(message);
+            Clients.Client(conID).singleTableResponse(response);
+        }
 
 
+        public void sendQuery(string message, string conID)
+        {
+            List<object[]> response = getQuery(message);
+            Clients.Client(conID).getTables(response);
+        }
+
+        public static void db_OnChange()
+        {
+            IHubContext context = GlobalHost.ConnectionManager.GetHubContext<ChatHub>();
+            context.Clients.All.refresh();
+        }
+
+        public void executeUserCommand(string userID, string query)
+        {
+
+            Debug.WriteLine(query);
+
+            using (SqlConnection conn = new SqlConnection())
+            {
+                conn.ConnectionString = "SERVER=włochu-ps2.azurewebsites.net;DATABASE=mk195000;USER ID=michal7018;PASSWORD=Michal7011;";
+
+                conn.Open();
+
+                if (conn.State == ConnectionState.Closed)
+                    conn.Open();
+
+                if (query.Contains("select"))
+                {
+                    List<object[]> response = getQuery(query);
+
+                    Debug.WriteLine(response);
+                    Clients.Client(userID).userCommandResponse(response);
+                }
+                else
+                {
+                    SqlCommand command = new SqlCommand(query);
+                    int result = -1;
+
+                    result = command.ExecuteNonQuery();
+
+                    Clients.Client(userID).clientMessage("Error!");
+                }
+            }
+        }
 
 
+        public void insertRow(string userID, string tableName, string param1, string param2 = null)
+        {
+
+            bool refreshFlag = false;
+
+            using (SqlConnection conn = new SqlConnection())
+            {
+                conn.ConnectionString = "SERVER=włochu-ps2.azurewebsites.net;DATABASE=mk195000;USER ID=michal7018;PASSWORD=Michal7011;";
+
+                conn.Open();
+
+                SqlCommand command = null;
+
+                if (conn.State == ConnectionState.Closed)
+                    conn.Open();
+
+                switch (tableName)
+                {
+                    case "czass":
+                        command = new SqlCommand("INSERT INTO czass (id, czass) VALUES (@0, @1)", conn);
+                        command.Parameters.Add(new SqlParameter("0", param1));
+                        command.Parameters.Add(new SqlParameter("1", param2));
+                        break;
+
+                    case "msg":
+                        command = new SqlCommand("INSERT INTO msg (msg) VALUES (@0)", conn);
+                        command.Parameters.Add(new SqlParameter("0", param1));
+
+                        break;
+                }
+
+                int result = -1;
+
+                try
+                {
+                    if (param1 == "")
+                        throw new Exception();
+                    result = command.ExecuteNonQuery();
+                }
+                catch (Exception e)
+                {
+                    Clients.Client(userID).clientMessage("Error inserting  data into Database!");
+                    refreshFlag = true;
+                }
+
+                if (result < 0)
+                {
+                    Clients.Client(userID).clientMessage("Error inserting data into Database!");
+                    refreshFlag = true;
+                }
+            }
+
+            if (!refreshFlag)
+                db_OnChange();
+        }
+
+
+        public void deleteRow(string userID, string tableName, string param1 = null)
+        {
+            bool refreshFlag = false;
+
+            using (SqlConnection conn = new SqlConnection())
+            {
+                conn.ConnectionString = "SERVER=wlochu321.azurewebsites.net;DATABASE=mk195000;USER ID=michal7018;PASSWORD=Michal7011;";
+
+                conn.Open();
+
+                SqlCommand command = null;
+
+                if (conn.State == ConnectionState.Closed)
+                    conn.Open();
+
+                switch (tableName)
+                {
+                    case "czass":
+                        command = new SqlCommand("DELETE FROM czass WHERE id=(@0)", conn);
+                        command.Parameters.Add(new SqlParameter("0", param1));
+                        break;
+
+                    case "msg":
+                        command = new SqlCommand("DELETE FROM msg WHERE id=(@0)", conn);
+                        command.Parameters.Add(new SqlParameter("0", param1));
+
+                        break;
+                }
+
+                int result = -1;
+
+                try
+                {
+                    if (param1 == "")
+                        throw new Exception();
+                    result = command.ExecuteNonQuery();
+
+                }
+                catch (Exception e)
+                {
+                    Clients.Client(userID).clientMessage("Error during deleting data into Database!");
+                    refreshFlag = true;
+                }
+
+                if (result < 0)
+                {
+                    Clients.Client(userID).clientMessage("Error during deleting data into Database!");
+                    refreshFlag = true;
+                }
+            }
+
+            if (!refreshFlag)
+                db_OnChange();
+        }
+
+        public void editRow(string userID, string tableName, string param1 = null, string param2 = null, string param3 = null)
+        {
+            bool refreshFlag = false;
+
+            using (SqlConnection conn = new SqlConnection())
+            {
+                conn.ConnectionString = "SERVER=wlochu321.azurewebsites.net;DATABASE=mk195000;USER ID=michal7018;PASSWORD=Michal7011;";
+
+                conn.Open();
+
+                SqlCommand command = null;
+
+                if (conn.State == ConnectionState.Closed)
+                    conn.Open();
+
+                switch (tableName)
+                {
+                    case "czass":
+                        command = new SqlCommand("UPDATE czass SET id=(@1), czas=(@2) WHERE id_czas=(@0)", conn);
+                        command.Parameters.Add(new SqlParameter("0", param1));
+                        command.Parameters.Add(new SqlParameter("1", param2));
+                        break;
+
+                    case "msg":
+                        command = new SqlCommand("UPDATE msg SET msgg=(@1) WHERE id=(@0)", conn);
+                        command.Parameters.Add(new SqlParameter("0", param1));
+                        command.Parameters.Add(new SqlParameter("1", param2));
+
+                        break;
+
+                }
+
+                int result = -1;
+
+                try
+                {
+                    if (param1 == "" || param2 == "")
+                        throw new Exception();
+                    result = command.ExecuteNonQuery();
+                }
+                catch (Exception e)
+                {
+                    Clients.Client(userID).clientMessage("Error during updating data into Database!");
+                    refreshFlag = true;
+
+                }
+
+                if (result < 0)
+                {
+                    Clients.Client(userID).clientMessage("Error during updating data into Database!");
+                    refreshFlag = true;
+                }
+
+            }
+
+            if (!refreshFlag)
+                db_OnChange();
+        }
 
 
 
